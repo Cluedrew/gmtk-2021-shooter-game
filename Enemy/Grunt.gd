eeb40cc496bae9e679953e1b66ab0d3973a24fc7
Cluedrew Kenfar Ink <cluedrew.of.ink@gmail.com>
2021-06-12 14:27:45 -0400

    The Grunt now has health, but does nothing when it dies.

--- Enemy/Grunt.gd
+++ Enemy/Grunt.gd
@@ -2,9 +2,10 @@ extends Area2D
 
 onready var speed: float = 100
 onready var player: Area2D = get_player()
+var max_health = 10
+onready var cur_health = max_health
 
-func _ready():
-	pass
+signal out_of_health
 
 func _process(delta: float):
 	var velocity: Vector2 = position.direction_to(player.position)
@@ -13,3 +14,8 @@ func _process(delta: float):
 func get_player():
 	# Brittle, must be a sibling of the Player.
 	return get_node("../Player")
+
+func apply_damage(damage: int):
+	cur_health -= clamp(0, damage, max_health)
+	if 0 == cur_health:
+		emit_signal("out_of_health")

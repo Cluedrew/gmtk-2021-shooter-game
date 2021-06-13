f8c9eba808d45e528fd6a7b9d24e169a84049fd5
Cluedrew Kenfar Ink <cluedrew.of.ink@gmail.com>
2021-06-13 13:00:03 -0400

    Quickly added a way to show player/boss health.

--- Misc/BasicLevel.gd
+++ Misc/BasicLevel.gd
@@ -9,3 +9,32 @@ func _ready():
 			[GameOver])
 	if OK != err:
 		print("connect failed: ", err)
+
+# TODO: pretty this up.
+onready var health_label = $Label
+var max_player_health = 0
+var cur_player_health = 0
+var max_boss_health = 0
+var cur_boss_health = 0
+
+func update_health_label():
+	var text = """
+	Player Health: {pc}/{pm}
+	Boss Health: {bc}/{bm}
+	"""
+	health_label.text(text.format({
+		"pc": cur_player_health,
+		"pm": max_player_health,
+		"bc": cur_boss_health,
+		"bm": max_boss_health,
+	}))
+
+func set_player_heath(cur_health, max_health):
+	cur_player_health = cur_health
+	max_player_health = max_health
+	update_health_label()
+
+func set_boss_health(cur_health, max_health):
+	cur_boss_health = cur_health
+	max_boss_health = max_health
+	update_health_label()

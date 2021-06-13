c9b28c3a7d75c5df28d07ccf6fa17f2faa4d2338
jchilibe-IB <38446044+Moocowsgomoo@users.noreply.github.com>
2021-06-13 12:13:42 -0600

    Fix occasional crash on boss death

--- Enemy/Boss.gd
+++ Enemy/Boss.gd
@@ -11,5 +11,5 @@ func get_player():
 	return null
 
 func apply_damage(damage: int):
+	if (cur_health - damage > 0): get_node("/root/Main/Level").set_boss_health(cur_health - damage,max_health)
 	.apply_damage(damage)
-	get_node("/root/Main/Level").set_boss_health(cur_health,max_health)
